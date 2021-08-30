82b58b6bd1b4c5cad76b9ba586a5567ec34c5aee
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-30 18:21:03 -0400

    completed mvp

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.post("/api/users", (req, res) => {
   const input = req.body;
   if (!input.name || !input.bio) {
     res.status(400).json({
-      message: "provide name and bio ",
+      message: "Please provide name and bio for the user",
     });
   } else {
     userFunctions
@@ -20,7 +20,9 @@ server.post("/api/users", (req, res) => {
       })
       .catch((err) => {
         console.log(err);
-        res.status(500).json({ message: err.message });
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+        });
       });
   }
 });
@@ -29,12 +31,12 @@ server.get("/api/users", async (req, res) => {
   userFunctions
     .find()
     .then((users) => {
-      //   console.log(users);
       res.status(200).json(users);
     })
     .catch((err) => {
-      //   console.log(err);
-      res.status(500).json({ message: err.message });
+      res
+        .status(500)
+        .json({ message: "The users information could not be retrieved" });
     });
 });
 
@@ -44,14 +46,18 @@ server.get("/api/users/:id", (req, res) => {
     .findById(id)
     .then((user) => {
       if (!user) {
-        res.status(404).json({ message: "does not exist" });
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
       } else {
         res.status(200).json(user);
       }
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: err.message });
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved" });
     });
 });
 
@@ -63,12 +69,14 @@ server.delete("/api/users/:id", (req, res) => {
       if (user) {
         res.status(200).json(user);
       } else {
-        res.status(404).json({ message: "does not exist" });
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
       }
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: "The user could not be removed" });
     });
 });
 
@@ -76,19 +84,26 @@ server.put("/api/users/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
   if (!changes.name || !changes.bio) {
-    res.status(400).json({ message: "provide name and bio" });
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user" });
   } else {
     userFunctions
       .update(id, changes)
       .then((user) => {
         if (user) {
-          res.status(201).json(user);
+          res.status(200).json(user);
         } else {
-          res.status(404).json({ message: "does not exist" });
+          res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist" });
         }
       })
       .catch((err) => {
         console.log(err);
+        res
+          .status(500)
+          .json({ message: "The user information could not be modified" });
       });
   }
 });
