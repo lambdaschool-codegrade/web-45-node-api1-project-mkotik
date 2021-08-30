bd28dccc7d3a91c21215f5d6464b3f8fe8711e36
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-30 16:04:37 -0400

    server listening

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const server = express();
+module.exports = server; // EXPORT YOUR SERVER instead of {}
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get("/", (req, res) => {
+  console.log("hey");
+  res.status(200).json("hello");
+});
