0b43f439ddf8144253599ef1d1710e8824d48c7b
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-30 17:36:14 -0400

    completed post /api/users endpoint

--- api/server.js
+++ api/server.js
@@ -4,8 +4,25 @@ const server = express();
 const userFunctions = require("./users/model.js");
 module.exports = server; // EXPORT YOUR SERVER instead of {}
 
-server.post("/api/users", () => {
-  console.log("create a user");
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  const input = req.body;
+  if (!input.name || !input.bio) {
+    res.status(400).json({
+      message: "provide name and bio ",
+    });
+  } else {
+    userFunctions
+      .insert(input)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+      });
+  }
 });
 
 server.get("/api/users", async (req, res) => {
@@ -31,4 +48,8 @@ server.delete("api/users/:id", (res, req) => {
 
 server.put("api/users/:id", () => {
   console.log("updates a user with id");
+});
+
+server.get("*", (req, res) => {
+  res.status(404).send("endpoint not found");
 });
